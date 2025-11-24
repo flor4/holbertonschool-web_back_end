53a5449b9395175802424014dcdbb0cd4934c52c
flor4 <salanson.flora@outlook.fr>
2025-11-24 15:34:53 +0100

    fix: comment

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
+"""
+Return the concatenation of two strings.
+"""
+
+
 def concat(str1: str, str2:str) -> str:
-    """Return the concatenation of two strings."""
+    """
+    concatenate function.
+    """
     return str1 + str2
