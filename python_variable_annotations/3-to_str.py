4a6a2e8d6c16ef4d9093990972a196d034966b79
flor4 <salanson.flora@outlook.fr>
2025-11-24 15:40:51 +0100

    Fix: code mistake -> str

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -4,8 +4,8 @@ Return the string representation of a float.
 """
 
 
-def to_str(n: float) ->float:
+def to_str(n: float) -> str:
     """
     to_str function
     """
-    return str(n)
+    return (str(n))
