2955928247a66fb6983df21e59e6bac1a62d31b5
flor4 <salanson.flora@outlook.fr>
2025-11-24 15:58:19 +0100

    fix: bracket

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -4,8 +4,8 @@ Return the concatenation of two strings.
 """
 
 
-def concat(str1: str, str2:str) -> str:
+def concat(str1: str, str2: str) -> str:
     """
     concatenate function.
     """
-    return str1 + str2
+    return (str1 + str2)
