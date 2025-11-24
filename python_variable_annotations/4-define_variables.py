5654b3a922ee5f926a91cc07b2be756f18487a17
flor4 <salanson.flora@outlook.fr>
2025-11-24 15:42:15 +0100

    fix: comments

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
+"""
+i_understand_annotations is to verify if we understand, as simple as it's.
+"""
 
-""" i_understand_annotations is to verify if we understand, as simple as it's."""
+
 a: int = 1
 pi: float = 3.14
 i_understand_annotations: bool = True
