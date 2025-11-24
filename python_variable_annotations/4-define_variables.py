c47fadb9b6853faea9c62e795be01417707520a7
flor4 <salanson.flora@outlook.fr>
2025-11-24 14:17:07 +0100

    Define and annotate the following variables with the specified values

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+""" i_understand_annotations is to verify if we understand, as simple as it's."""
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
