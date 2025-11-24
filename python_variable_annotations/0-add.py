38c80214c4743577256f9a7ebe6f06108f6a7ec3
flor4 <salanson.flora@outlook.fr>
2025-11-24 15:32:19 +0100

    fix: comment

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 
-"""This module contains a function that returns the sum of two floats.
-It demonstrates type annotations and proper documentation."""
+"""
+This module contains a function that returns the sum of two floats.
+It demonstrates type annotations and proper documentation.
+"""
 
 
 def add(a: float, b: float) -> float:
+    """
+    add function
+    """
     return a + b
