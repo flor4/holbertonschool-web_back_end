cdaa63c6831dbb9865c0eef2d204e9764c869f71
flor4 <salanson.flora@outlook.fr>
2025-12-05 14:04:00 +0100

    fix: shebang

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 """
 Write a Python function that lists all documents in a collection
@@ -9,4 +9,5 @@ def list_all(mongo_collection):
     """
     lists all documents in a collection
     """
-    return mongo_collection.find()
+    resultat = mongo_collection.find()
+    return resultat
